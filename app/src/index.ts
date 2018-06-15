add15c28d658496e544fedfcc2b756dea265d185
Kuben <jordan.venant@gmail.com>
2018-06-15 21:18:53 +0200

    AJout de statsJS pour la vue deu nombre de FPS

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,22 @@
-console.log('in app');
+import * as THREE from 'three';
+
+const stats = require('../lib/stat.min.js')();
+stats.showPanel( 0 );
+document.body.appendChild( stats.dom );
+
+
+
+
+function animate() {
+
+    stats.begin();
+
+    // monitored code goes here
+
+    stats.end();
+
+    requestAnimationFrame( animate );
+
+}
+
+requestAnimationFrame( animate );
