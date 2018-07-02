53f0ec098b8aeccbc3c5326ab177aca1ddc2c099
Kubenic <jordan.venant@gmail.com>
2018-07-02 19:21:46 +0200

    fix image 404 problem

--- app/src/classes/grass.js
+++ app/src/classes/grass.js
@@ -4,15 +4,15 @@ import {MeshPhongMaterial, NearestFilter, TextureLoader} from 'three'
 const loader = new TextureLoader();
 
 // side of grass texture
-const side_texture = loader.load('../img/grass_side.png');
+const side_texture = loader.load('img/grass_side.png');
 side_texture.magFilter = NearestFilter;
 const grass_side = new MeshPhongMaterial({map: side_texture});
 // top of grass texture
-const top_texture = loader.load('../img/grass_top.png');
+const top_texture = loader.load('img/grass_top.png');
 top_texture.magFilter = NearestFilter;
 const grass_top = new MeshPhongMaterial({map: top_texture});
 // bottom of grass texture
-const bottom_texture = loader.load('../img/dirt.png');
+const bottom_texture = loader.load('img/dirt.png');
 bottom_texture.magFilter = NearestFilter;
 const grass_bottom = new MeshPhongMaterial({map: bottom_texture});
 
